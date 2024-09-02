a12d546c5e36751e84726ea1d291ef38fc3d407f
Andre Goncalves <andrefcgoncalves@gmail.com>
2024-09-02 18:37:20 +0100

    Add PredictionMarketV3Controller Contract

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import cors from 'cors';
 
 import { router } from './routes';
 import { queuesPath, queuesRouter } from './queues';
+import logger from 'morgan';
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.use(cors());
 app.use(express.json());
