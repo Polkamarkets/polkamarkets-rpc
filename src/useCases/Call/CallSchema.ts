cbf11b8020ef12d1f82cefa5dbcc0d8d852786f2
Ricardo Marques <ricardo.ds.marques@gmail.com>
2025-07-02 23:39:08 +0100

    feat(3.2): add PMV3 querier contract

--- src/useCases/Call/CallSchema.ts
+++ src/useCases/Call/CallSchema.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const callSchema = yup.object({
   contract: yup
     .mixed()
-    .oneOf(['predictionMarket', 'predictionMarketV2', 'predictionMarketV3', 'predictionMarketV3_2', 'predictionMarketV3Manager', 'predictionMarketV3Controller', 'erc20', 'realitio', 'achievements', 'voting', 'arbitration', 'arbitrationProxy'])
+    .oneOf(['predictionMarket', 'predictionMarketV2', 'predictionMarketV3', 'predictionMarketV3_2', 'predictionMarketV3Manager', 'predictionMarketV3Controller', 'predictionMarketV3Querier', 'erc20', 'realitio', 'achievements', 'voting', 'arbitration', 'arbitrationProxy'])
     .required('Contract is required!'),
   method: yup.string().required('Method is required!'),
   address: yup.string().required('Address is required!')
